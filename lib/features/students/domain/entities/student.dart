6a8db4e4567b43732692617a29384b9326668256
Sreeraj9764 <cnsreeraj97@gmail.com>
2024-07-24 09:03:05 +0530

    added student entity

---
+++ lib/features/students/domain/entities/student.dart
@@ -0,0 +1,12 @@
+class Student {
+  final int id;
+  final String name;
+  final int age;
+  final String email;
+
+  Student(
+      {required this.name,
+      required this.age,
+      required this.email,
+      required this.id});
+}
